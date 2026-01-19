4739b601ebfd5bfce5ff2d06b0d3413e3c98dfe9
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2026-01-19 22:10:07 +0530

    feat(auth): add jwt access token utility

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,4 @@
 import { app } from "./app.js";
-import { hashPassword, verifyPassword } from "./utils/auth/password.util.js";
 
 const PORT = process.env.PORT || 4000;
 
@@ -7,11 +6,6 @@ const start = async () => {
   try {
     await app.listen({ port: PORT });
     console.log(`Server is running at http://localhost:${PORT}`);
-    //pass.utils sanity check
-    const hashed = await hashPassword("test123");
-    console.log("Hashed:", hashed);
-    const isValid = await verifyPassword("test123", hashed);
-    console.log("Password valid:", isValid);
   } catch (error) {
     app.log.error(error);
     process.exit(1);
--- backend/src/utils/auth/password.util.ts
+++ backend/src/utils/auth/password.util.ts
@@ -5,6 +5,6 @@ export const hashPassword = async (password: string): Promise<string> => {
     return bcrypt.hash(password, SALT_ROUNDS)
 };
 
-export const verifyPassword = async (password: string, hashedPassword: string): Promise<string> => {
+export const verifyPassword = async (password: string, hashedPassword: string): Promise<boolean> => {
     return bcrypt.compare(password, hashedPassword);
 }
