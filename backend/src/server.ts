88f7fb11c6cfa9d1869106e315e4ab5566a8a174
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2026-01-19 22:00:17 +0530

    feat(auth): add password hashing utilities

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import { app } from "./app.js";
+import { hashPassword, verifyPassword } from "./utils/auth/password.util.js";
 
 const PORT = process.env.PORT || 4000;
 
@@ -6,6 +7,11 @@ const start = async () => {
   try {
     await app.listen({ port: PORT });
     console.log(`Server is running at http://localhost:${PORT}`);
+    //pass.utils sanity check
+    const hashed = await hashPassword("test123");
+    console.log("Hashed:", hashed);
+    const isValid = await verifyPassword("test123", hashed);
+    console.log("Password valid:", isValid);
   } catch (error) {
     app.log.error(error);
     process.exit(1);
